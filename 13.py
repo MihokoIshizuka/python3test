5d19a1089c38df469a92a9c96b1e26234400a68b
MihokoIshizuka <103006914+MihokoIshizuka@users.noreply.github.com>
2022-10-17 22:35:08 +0900

    for文

---
+++ 13.py
@@ -0,0 +1,12 @@
+def main():
+    for i in [1, 2, 3]:
+        print(i)
+    for i in range(1,11):
+        print(i)
+
+    for word in ["Python","R","SQL"]:
+        print(word, len(word))
+
+
+if __name__ == "__main__":
+    main()
