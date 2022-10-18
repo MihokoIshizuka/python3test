1ac4fd0f1b5a6657bc44b9f9e97d417165ddaae8
MihokoIshizuka <mi.cobito.1013otter@gmail.com>
2022-10-18 21:04:27 +0900

    forとelse

---
+++ 16.py
@@ -0,0 +1,14 @@
+def main():
+    for i in range(10):
+        if i == 5:
+            break
+        print(i)
+
+    for i in range(10):
+        print(i)
+    else:
+        print("繰り返しが最後まで実行されました")
+
+
+if __name__ == "__main__":
+    main()
